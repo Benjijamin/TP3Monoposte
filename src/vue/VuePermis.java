84dc7389fe6291a547fce7d0c323628eaaa15130
benjamincd <benjicourriel@gmail.com>
2020-05-01 15:49:16 -0400

    Tous les ids sont associés dans la vue

--- src/vue/VuePermis.java
+++ src/vue/VuePermis.java
@@ -3,16 +3,105 @@ package vue;
 import java.io.IOException;
 
 import controleur.ICtrl;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.*;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
-public class VuePermis implements IVue{
+public class VuePermis implements IVue {
 	private ICtrl ctrl;
 	private Scene scene;
-	private Pane root;
+	private VBox root;
 
 	// CONTROLS
+	
+	//menu
+	@FXML
+	private MenuItem menuExporterXML;
+	@FXML
+	private MenuItem menuImporterCSV;
+	@FXML
+	private MenuItem menuQuitter;
+	@FXML
+	private MenuItem menuNouveau;
+	@FXML
+	private MenuItem menuAjouter;
+	@FXML
+	private MenuItem menuModifier;
+	@FXML
+	private MenuItem menuSupprimer;
+	@FXML
+	private MenuItem menuGererTerritoires;
+	@FXML
+	private MenuItem menuGererTypes;
+	@FXML
+	private MenuItem menuAide;
+	
+	//Liste des permis
+	@FXML
+	private TextField fieldRecherche;
+	@FXML
+	private Button buttonRecherche;
+	@FXML
+	private ListView listViewPermis;
+	
+	//AnchorPane du dessus
+	@FXML
+	private TextField fieldNumero;
+	@FXML
+	private ChoiceBox choiceBoxTerritoire;
+	@FXML
+	private Button buttonTerritoire;
+	@FXML
+	private DatePicker datePickerDateDebut;
+	@FXML
+	private DatePicker datePickerDateFin;
+	@FXML
+	private Button buttonSupprimer;
+	
+	//AnchorPane du bas
+	@FXML
+	private TextField fieldNom;
+	@FXML
+	private DatePicker datePickerNaissance;
+	@FXML
+	private CheckBox checkBoxVaccine;
+	@FXML
+	private CheckBox checkBoxSterelise;
+	@FXML
+	private CheckBox checkBoxMicropuce;
+	@FXML
+	private CheckBox checkBoxDangereux;
+	@FXML
+	private ChoiceBox choiceBoxType;
+	@FXML
+	private TextField fieldPoids;
+	@FXML
+	private Button buttonType;
+	@FXML
+	private ComboBox comboBoxCouleur;
+	
+	//Radiobuttons
+	@FXML
+	private ToggleGroup toggleGroup;
+	@FXML
+	private RadioButton choiceMale;
+	@FXML
+	private RadioButton choiceFemelle;
+	@FXML
+	private RadioButton choiceInconnu;
+	
+	//Boutons
+	@FXML
+	private Button buttonNouveau;
+	@FXML
+	private Button buttonAjouter;
+	@FXML
+	private Button buttonModifier;
+	@FXML
+	private Button buttonQuitter;
+	@FXML
+	private Button buttonAide;
 
 	/**
 	 * @param ctrl : controleur de l'application.
@@ -36,7 +125,7 @@ public class VuePermis implements IVue{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Scene getScene() {
 		return scene;
 	}
