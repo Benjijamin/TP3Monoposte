d49d6a4bef32420803971cc643bbb80b771e6c36
JeanSamGirard <jeansam@live.ca>
2020-05-07 11:44:59 -0400

    Configuration hibernate

--- src/manager/PermisManager.java
+++ src/manager/PermisManager.java
@@ -1,5 +1,10 @@
 package manager;
 
+import modele.Permis;
+
 public class PermisManager {
 
+	public void ajouterPermis(Permis permis) {
+		
+	}
 }
--- src/modele/Territoire.java
+++ src/modele/Territoire.java
@@ -1,5 +1,36 @@
 package modele;
 
 public class Territoire {
+	private int id;
+	private String nom;
+
+	public Territoire() {
+	}
+
+	public Territoire(int id, String nom) {
+		this.id = id;
+		this.nom = nom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Territoire [id=" + id + ", nom=" + nom + "]";
+	}
 
 }
