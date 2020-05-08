fd2ccf13b7cb3e35c01ada600e8b0700c76e99de
JeanSamGirard <jeansam@live.ca>
2020-05-08 13:32:20 -0400

    Changement des modal pour ne pas modifier les types/territoires

--- src/manager/TypeManager.java
+++ src/manager/TypeManager.java
@@ -1,5 +1,17 @@
 package manager;
 
-public class TypeManager {
+import org.hibernate.Session;
 
+import modele.Type;
+import util.HibernateUtil;
+
+public class TypeManager {
+	
+	public void ajouterType(String type) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(new Type(0,type));
+		session.getTransaction().commit();
+	}
+	
 }
