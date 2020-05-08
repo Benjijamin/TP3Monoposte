3fb9924c28a2b5ece4215554f5972db94f865602
JeanSamGirard <jeansam@live.ca>
2020-05-08 13:59:24 -0400

    TypeManager completé

--- src/manager/TypeManager.java
+++ src/manager/TypeManager.java
@@ -1,6 +1,9 @@
 package manager;
 
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import modele.Type;
 import util.HibernateUtil;
@@ -12,6 +15,23 @@ public class TypeManager {
 		session.beginTransaction();
 		session.save(new Type(0,type));
 		session.getTransaction().commit();
+	}
+	
+	public void modifierType(String typebefore, String typeafter) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Type temp = (Type) session.createCriteria(Type.class).add(Restrictions.eq("nom", typebefore)).uniqueResult();
+		temp.setNom(typeafter);
+		session.update(temp);
+		session.getTransaction().commit();
+	}
+	
+	public List<Type> getTypes(){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		List<Type> types = session.createCriteria(Type.class).list();
+		session.close();
+		return types;
 	}
 	
 }
