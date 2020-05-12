09eb2d9ebe75b268595808734d40975b6dd42220
JeanSamGirard <jeansam@live.ca>
2020-05-12 18:26:44 -0400

    Manager Territoire

--- src/manager/TerritoireManager.java
+++ src/manager/TerritoireManager.java
@@ -1,5 +1,37 @@
 package manager;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import modele.Territoire;
+import util.HibernateUtil;
+
 public class TerritoireManager {
 
+	public void ajouterTerritoire(String territoire) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(new Territoire(0,territoire));
+		session.getTransaction().commit();
+	}
+	
+	public void modifierTerritoire(String territoirebefore, String territoireafter) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Territoire temp = (Territoire) session.createCriteria(Territoire.class).add(Restrictions.eq("nom", territoirebefore)).uniqueResult();
+		temp.setNom(territoireafter);
+		session.update(temp);
+		session.getTransaction().commit();
+	}
+	
+	public List<Territoire> getTerritoire(){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		List<Territoire> territoires = session.createCriteria(Territoire.class).list();
+		session.close();
+		return territoires;
+	}
+	
 }
